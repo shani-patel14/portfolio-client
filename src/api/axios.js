6841f27974a0064f9873cb969111e9f45d6edbfc
shani <shanipatel5008@gmail.com>
2026-05-14 22:32:01 +0530

    fix api route to lowercase

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'https://portfolio-server-1-qy0x.onrender.com/api/projects',
+  baseURL: process.env.REACT_APP_API_URL || 'https://portfolio-server-1-qy0x.onrender.com',
 })
 export default API; 
