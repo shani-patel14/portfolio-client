33bf23eb4e480d55c0514b67d457ff5693201fd2
shani <shanipatel5008@gmail.com>
2026-06-10 22:28:30 +0530

    Updated portfolio website

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,14 @@
-import axios from 'axios';
+// import axios from 'axios';
+// const API = axios.create({
+//   baseURL: 'https://portfolio-server-1-qy0x.onrender.com/api',
+  
+// })
+// export default API; 
+
+import axios from 'axios'
+
 const API = axios.create({
-  baseURL: 'https://portfolio-server-1-qy0x.onrender.com',
-})
-export default API; 
+  baseURL: 'https://portfolio-server-2-aeq8.onrender.com/api'
+});
+
+export default API
